4334840378b59980a16e950116f9062525c4fd1a
Firas <firasaldabbas+git@gmail.com>
2019-10-09 00:44:53 -0400

    model training done

--- text_classify.py
+++ text_classify.py
@@ -31,8 +31,8 @@ reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 # Want to pad everything so the set input/output neurons fit, though I guess a general intelligence wouldn't need it. But what am I, a god already? chill.
 # So, either choose the longest review, or pick an arbitrarily length; cut off reviews longer, or pad ones shorter.
-train_data = keras.preprocessing.sequence.pad_sequence(train_data, value=word_index["<PAD>"], padding="post", maxLen=256)
-test_data = keras.preprocessing.sequence.pad_sequence(test_data, value=word_index["<PAD>"], padding="post", maxLen=256)
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=256)
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
@@ -46,8 +46,21 @@ def decode_review(text):
 ###
 
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Embedding(10000, 16))            # Embedding into 16D vectors for word closeness
+model.add(keras.layers.GlobalAveragePooling1D())        # Average of the embedded vals
+model.add(keras.layers.Dense(16, activation="relu"))    # find word patterns
 model.add(keras.layers.Dense(1, activation="sigmoid"))  # Final output; good or bad?
 
+model.summary()
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+y_val = train_labels[:10000]
+
+x_train = train_data[10000:]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+results = model.evaluate(test_data, test_labels)
+
+print(results)
