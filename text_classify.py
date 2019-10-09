3d09a2a671b9e136d9521d3df4cb81f29c974a49
Firas <firasaldabbas+git@gmail.com>
2019-10-09 00:17:16 -0400

    model setup for the text classification

--- text_classify.py
+++ text_classify.py
@@ -45,3 +45,9 @@ def decode_review(text):
 # MODEL
 ###
 
+model = keras.Sequential()
+model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))  # Final output; good or bad?
+
